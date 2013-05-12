01c3ca9e516ea68728cb7b81d1fa5ae800095908
kronusme <kronusme@gmail.com>
2013-05-12 23:39:34 +0300

    Remove ChromePHP Dependence

--- view.php
+++ view.php
@@ -5,7 +5,6 @@ require_once('config.php');
 $_GET['url'] = url::decode((string)$_GET['url']);
 
 $url = new url($_GET['url']);
-ChromePhp::log($url);
 $pf = new page_factory();
 $page = $pf->get_page($url);
 $modified_content = $page->process();
