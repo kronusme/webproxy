d19e71b5e963befe34f85c33abb619c1757f6562
kronusme <kronusme@gmail.com>
2013-06-08 01:17:21 +0300

    Includes ->Src

--- config.php
+++ config.php
@@ -4,11 +4,11 @@ error_reporting(0);
 
 require_once('vendor/autoload.php');
 
-require_once('includes/class.url.php');
-require_once('includes/class.page_factory.php');
-require_once('includes/class.page.php');
-require_once('includes/class.basic_page.php');
-require_once('includes/class.css_page.php');
-require_once('includes/class.html_page.php');
-require_once('includes/class.request.php');
-require_once('includes/simple_html_dom.php');
+require_once('src/class.url.php');
+require_once('src/class.page_factory.php');
+require_once('src/class.page.php');
+require_once('src/class.basic_page.php');
+require_once('src/class.css_page.php');
+require_once('src/class.html_page.php');
+require_once('src/class.request.php');
+require_once('src/simple_html_dom.php');
